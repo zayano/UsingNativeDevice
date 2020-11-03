904edc9783df79637b059824f7893d520e49d505
Reza Aditya <adityareza46@gmail.com>
2020-11-03 16:56:24 +0700

    Managing Data & Images via the Provider Package

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:tutorial_native/screens/add_place_screen.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/great_places.dart';
+import '../screens/add_place_screen.dart';
 
 class PlacesListScreen extends StatelessWidget {
   const PlacesListScreen({Key key}) : super(key: key);
@@ -18,8 +21,26 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: CircularProgressIndicator(),
+      body: Consumer<GreatPlaces>(
+        child: Center(
+          child: const Text('Got no places yet, start adding some!'),
+        ),
+        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+            ? ch
+            : ListView.builder(
+                itemCount: greatPlaces.items.length,
+                itemBuilder: (ctx, i) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: FileImage(
+                      greatPlaces.items[i].image,
+                    ),
+                  ),
+                  title: Text(greatPlaces.items[i].title),
+                  onTap: () {
+                    // test
+                  },
+                ),
+              ),
       ),
     );
   }
--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -6,6 +6,10 @@ import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
+  final Function onSelectImage;
+
+  ImageInput(this.onSelectImage);
+
   @override
   _ImageInputState createState() => _ImageInputState();
 }
@@ -26,6 +30,7 @@ class _ImageInputState extends State<ImageInput> {
     final fileImage = File(imageFile.path);
     final fileName = path.basename(fileImage.path);
     final savedImage = await fileImage.copy('${appDir.path}/$fileName');
+    widget.onSelectImage(savedImage);
   }
 
   @override

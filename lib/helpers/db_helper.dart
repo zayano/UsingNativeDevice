7584aff52fb4d7434e572174d752f9552da3760c
Reza Aditya <adityareza46@gmail.com>
2020-11-12 10:15:56 +0700

    Storing & Fetching Data with SQLite

--- lib/helpers/db_helper.dart
+++ lib/helpers/db_helper.dart
@@ -1,10 +1,11 @@
 import 'package:sqflite/sqflite.dart' as sql;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqlite_api.dart';
 
 class DBHelper {
-  static Future<void> insert(String table, Map<String, Object> data) async {
+  static Future<Database> database() async {
     final dbPath = await sql.getDatabasesPath();
-    final sqlDb = await sql.openDatabase(
+    return sql.openDatabase(
       path.join(dbPath, 'places.db'),
       onCreate: (db, version) {
         return db.execute(
@@ -12,11 +13,19 @@ class DBHelper {
       },
       version: 1,
     );
+  }
 
-    await sqlDb.insert(
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final db = await DBHelper.database();
+    db.insert(
       table,
       data,
       conflictAlgorithm: sql.ConflictAlgorithm.replace,
     );
+  }
+
+  static Future<List<Map<String, dynamic>>> getData(String table) async {
+    final db = await DBHelper.database();
+    return db.query(table);
   }
 }
